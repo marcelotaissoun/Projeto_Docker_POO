ea2e62daddf5309d9217bfe15ba5e7f20ffc4efb
dev-marcoslsilva <marcosandre1309.profissional@gmail.com>
2026-07-23 15:25:17 -0300

    created the repository folder and one file

---
+++ microkernel/repository/src/main/java/br/edu/ifba/inf008/repository/CartRepository.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.inf008.repository;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import br.edu,ifba.inf008.model.Cart;
+import br.edu.ifba.inf008.util.HibernateUtil;
+
+import java.util.List;
+
+public class CartRepository {
+    
+}
