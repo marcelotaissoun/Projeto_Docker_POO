4a375dc4d641f51f87351cb2966073dd3f8873f0
Marcelo Taissoun <marcelotaissoun@gmail.com>
2026-07-22 22:16:39 -0300

    Add OrderItem class for order management

---
+++ microkernel/domain/OrderItem.java
@@ -0,0 +1,29 @@
+import java.util.BigDecimal;
+
+public class OrderItem {
+    private Product product;
+    private int quantity;
+    private BigDecimal price;
+
+    public OrderItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+        this.price = product.getPrice();
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public BigDecimal getSubtotal() {
+        return price.multiply(BigDecimal.valueOf(quantity));
+    }
+}
